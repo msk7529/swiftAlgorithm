7080db095fb5cc517d3f1dfcc0fa9576ba183f1a
Kim Min Seop <newmsk7529@gmail.com>
2021-04-12 02:01:07 +0900

    백준 1065번(한수) - 구현

--- main.swift
+++ main.swift
@@ -2,12 +2,39 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by kakao on 2021/04/12.
+//  Created by MinSeop on 2021/04/12.
 //
-
 import Foundation
 
-readLine()
-print("Hello, World!")
+let num: Int = Int(readLine()!)!
+var result: Int = 0
+
+if num < 100 {
+    print(num)
+} else {
+    result = 99
+    for number in 100...num {
+        if check(number) {
+            result += 1
+        }
+    }
+    print(result)
+}
+
+func check(_ num: Int) -> Bool {
+    if num == 1000 {
+        return false
+    }
+    
+    let firstNum: Int = num / 100
+    let secondNum : Int = (num - firstNum * 100) / 10
+    let thirdNum: Int = num - firstNum * 100 - secondNum * 10
+    
+    let firstDiv: Int = firstNum - secondNum
+    let secondDiv: Int = secondNum - thirdNum
+    
+    if firstDiv == secondDiv { return true }
+    return false
+}
 
 
