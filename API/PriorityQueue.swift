6b04458aa61c247ee887b78c5765c2a7e2b8b8b7
Kim Min Seop <newmsk7529@gmail.com>
2021-04-23 01:39:51 +0900

    백준 1753번(최단경로) - pq를 이용한 다익스트라

---
+++ API/PriorityQueue.swift
@@ -0,0 +1,79 @@
+
+public struct PriorityQueue<T> {
+    var nodes: [T] = []
+    let comparer: (T,T) -> Bool
+
+    init(comparer: @escaping (T,T) -> Bool) {
+        self.comparer = comparer
+    }
+
+    var isEmpty: Bool {
+        return nodes.isEmpty
+    }
+
+    func peek() -> T? {
+        return nodes.first
+    }
+
+    mutating func enqueue(_ element: T) {
+        var index: Int = nodes.count
+
+        nodes.append(element)
+
+        while index > 0, !comparer(nodes[index],nodes[(index - 1) / 2]) {
+            nodes.swapAt(index, (index - 1) / 2)
+            index = (index - 1) / 2
+        }
+    }
+
+    mutating func dequeue() -> T? {
+        guard !nodes.isEmpty else {
+            return nil
+        }
+
+        if nodes.count == 1 {
+            return nodes.removeFirst()
+        }
+
+        let result: T? = nodes.first
+        nodes.swapAt(0, nodes.count - 1)
+        _ = nodes.popLast()
+
+        var index = 0
+
+        while index < nodes.count {
+            let left: Int = index * 2 + 1
+            let right: Int = left + 1
+
+            if right < nodes.count {
+                if comparer(nodes[left], nodes[right]),
+                   !comparer(nodes[right], nodes[index]) {
+                    nodes.swapAt(right, index)
+                    index = right
+                } else if !comparer(nodes[left], nodes[index]){
+                    nodes.swapAt(left, index)
+                    index = left
+                } else {
+                    break
+                }
+            } else if left < nodes.count {
+                if !comparer(nodes[left], nodes[index]) {
+                    nodes.swapAt(left, index)
+                    index = left
+                } else {
+                    break
+                }
+            } else {
+                break
+            }
+        }
+
+        return result
+    }
+}
+
+extension PriorityQueue where T: Comparable {
+    init() {
+        self.init(comparer: >)
+    }
+}
