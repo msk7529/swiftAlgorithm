3acb4b7227d9ff8ac77a0a931b59d84b72ad32c7
Kim Min Seop <msk7529@naver.com>
2021-04-27 03:38:48 +0900

    백준 1987번(알파벳) - dfs 백트래킹. 시간초과(swift 맞은사람 없음). 다른언어로도 visited 배열 정의 잘해야 시간초과 발생 안함.

---
+++ 백준1987_알파벳.swift
@@ -0,0 +1,56 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/04/27.
+//
+
+var R: Int = 0
+var C: Int = 0
+
+let input: [Int] = readLine()!.split(separator: " ").map { Int(String($0))! }
+R = input[0]
+C = input[1]
+
+let dir: [(Int, Int)] = [(1,0),(-1,0),(0,1),(0,-1)]
+let AofAscii: Int = Int(Character("A").asciiValue!)
+var graph: [[Character]] = Array(repeating: [], count: R)
+var visited: [Int] = Array(repeating: 0, count: 27)
+var result: Int = 0
+
+for i in 0..<R {
+    let line: [Character] = readLine()!.map { Character(String($0)) }
+    graph[i].append(contentsOf: line)
+}
+
+visited[Int(graph[0][0].asciiValue!) - AofAscii] = 1
+
+func dfs(_ y: Int, _ x: Int) {
+    let currIdx: Int = Int(graph[y][x].asciiValue!) - AofAscii
+    
+    for currDir in dir {
+        let nextY = y + currDir.0
+        let nextX = x + currDir.1
+
+        if isValid(nextY, nextX) && visited[Int(graph[nextY][nextX].asciiValue!) - AofAscii] == 0 {
+            let nextIdx: Int = Int(graph[nextY][nextX].asciiValue!) - AofAscii
+            
+            visited[nextIdx] = visited[currIdx] + 1
+            result = max(result, visited[nextIdx])
+            dfs(nextY, nextX)
+            visited[nextIdx] = 0
+        }
+    }
+}
+
+func isValid(_ y: Int, _ x: Int) -> Bool {
+    if y < 0 || y >= R || x < 0 || x >= C {
+        return false
+    }
+    return true
+}
+
+dfs(0, 0)
+print(result)
+
+
