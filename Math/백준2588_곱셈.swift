48acf2fdc89e124c4f61832bc9b0785322c0c223
Kim Min Seop <msk7529@naver.com>
2021-05-06 03:21:03 +0900

    백준 2588번(곱셈) - 수학

---
+++ Math/백준2588_곱셈.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/06.
+//
+/*
+import Foundation
+
+let a = readLine()!
+let aNum = Int(a)!
+let b = readLine()!
+let bNum = Int(b)!
+
+print(aNum * Int(String(b.last!))!)
+print(aNum * Int(String(b[b.index(b.startIndex, offsetBy: 1)]))!)
+print(aNum * Int(String(b.first!))!)
+print(aNum * bNum)
+*/
