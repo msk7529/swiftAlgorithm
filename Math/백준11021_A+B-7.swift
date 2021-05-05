32bda29cd450260c45858b1130552b0f5ad474a0
Kim Min Seop <msk7529@naver.com>
2021-05-06 03:09:13 +0900

    백준 11021번(A+B - 7) - 수학

---
+++ Math/백준11021_A+B-7.swift
@@ -0,0 +1,14 @@
+//
+//  백준11021_A+B-7.swift
+//  Algorithm
+//
+//  Created by kakao on 2021/05/06.
+//
+/*
+let caseStr: String = "Case #"
+let T = Int(readLine()!)!
+for i in 1...T {
+    let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+    print("\(caseStr)\(i): \(line[0] + line[1])")
+}
+*/
