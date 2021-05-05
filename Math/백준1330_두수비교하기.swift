2431e5486ff93f00df130c6d08cdf92792edcd76
Kim Min Seop <msk7529@naver.com>
2021-05-06 01:53:42 +0900

    백준 1330번(두 수 비교하기) - 수학

---
+++ Math/백준1330_두수비교하기.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/06.
+//
+//import Foundation
+/*
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+let a = input[0]
+let b = input[1]
+
+if a > b {
+    print(">")
+} else if a == b {
+    print("==")
+} else {
+    print("<")
+}
+*/
