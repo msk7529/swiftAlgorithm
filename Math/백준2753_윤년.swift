e38d89ce9a87fec9c1f64f695c4beea5edb46d86
Kim Min Seop <msk7529@naver.com>
2021-05-06 03:01:46 +0900

    백준 11021번(윤년) - 수학

---
+++ Math/백준2753_윤년.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/06.
+//
+//import Foundation
+/*
+let year = Int(readLine()!)!
+if year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) {
+    print("1")
+} else {
+    print("0")
+}
+*/
