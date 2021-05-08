b591cb6715ac89fcf444adf90bbff11d5d38b7e5
Kim Min Seop <msk7529@naver.com>
2021-05-08 22:15:42 +0900

    백준 11022번(A+B - 8) - 수학

---
+++ Math/백준11022_A+B-8.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/08.
+//
+//import Foundation
+/*
+let T: Int = Int(readLine()!)!
+for i in 1...T {
+    let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+    print("Case #\(i): \(line[0]) + \(line[1]) = \(line[0]+line[1])")
+}
+*/
