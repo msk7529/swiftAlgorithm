e3b8661b88464ffaa9c18676c3b4ddf5796d5af6
Kim Min Seop <msk7529@naver.com>
2021-05-09 18:42:09 +0900

    백준 3052번(나머지) - 수학

---
+++ Math/백준3052_나머지.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/09.
+//
+/*
+var num: Set<Int> = .init()
+for _ in 0..<10 {
+    num.insert(Int(readLine()!)! % 42)
+}
+print(num.count)
+*/
