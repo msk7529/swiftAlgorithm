4217a534b53534c2ac6a58d7a5d20579eb516b16
Kim Min Seop <msk7529@naver.com>
2021-05-11 01:36:31 +0900

    백준 1712번(손익분기점) - 수학

---
+++ Math/백준1712_손익분기점.swift
@@ -0,0 +1,36 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/11.
+//
+/*
+var A: Double = 0
+var B: Double = 0
+var C: Double = 0
+var result: Double = 0.0
+var resultInt: Int = 0
+
+func input() {
+    let input = readLine()!.split(separator: " ").map { Double(String($0))! }
+    A = input[0]
+    B = input[1]
+    C = input[2]
+
+    if C - B == 0 {
+        print("-1")
+        return
+    }
+
+    result = A / (C - B)
+    resultInt = Int(result) + 1
+
+    if resultInt < 0 {
+        print("-1")
+    } else {
+        print(resultInt)
+    }
+}
+
+input()
+*/
