ffb80a9763b7048ad3e8e7aad47f2a5b2e786b64
Kim Min Seop <msk7529@naver.com>
2021-05-10 14:07:28 +0900

    백준 2292번(벌집) - 수학

--- main.swift
+++ main.swift
@@ -5,3 +5,21 @@
 //  Created by MinSeop on 2021/05/09.
 //
 
+var N: Int = Int(readLine()!)!
+var result: Int = 1
+var loopCnt: Int = 1
+var endNum: Int = 1
+
+if N == 1 {
+    print("1")
+} else {
+    while true {
+        endNum += 6 * loopCnt
+        result += 1
+        loopCnt += 1
+        if endNum >= N {
+            print(result)
+            break
+        }
+    }
+}
