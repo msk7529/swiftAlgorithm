df7746374cf51c9979a8f9adee9091f069a9b66d
Kim Min Seop <msk7529@naver.com>
2021-05-11 21:35:52 +0900

    백준 14681번(사분면 고르기) - 수학

---
+++ Math/백준14681_사분면고르기.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/11.
+//
+/*
+let X: Int = Int(readLine()!)!
+let Y: Int = Int(readLine()!)!
+
+if X > 0 && Y > 0 {
+    print("1")
+} else if X < 0 && Y > 0 {
+    print("2")
+} else if X < 0 && Y < 0 {
+    print("3")
+} else {
+    print("4")
+}
+*/
