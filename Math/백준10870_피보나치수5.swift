006d3918c12bad560b883137487f318c4f368b55
Kim Min Seop <msk7529@naver.com>
2021-05-11 21:43:48 +0900

    백준 10870번(피보나치수 5) - 수학

---
+++ Math/백준10870_피보나치수5.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/11.
+//
+/*
+let n: Int = Int(readLine()!)!
+
+if n == 0 {
+    print("0")
+} else if n == 1 {
+    print("1")
+} else {
+    var F: [Int] = Array(repeating: 0, count: n+1)
+    F[1] = 1
+
+    for i in 2...n {
+        F[i] = F[i-1] + F[i-2]
+    }
+    print(F[n])
+}
+*/
--- main.swift
+++ main.swift
@@ -2,5 +2,5 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by MinSeop on 2021/05/10.
+//  Created by MinSeop on 2021/05/11.
 //
