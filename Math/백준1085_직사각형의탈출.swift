4ad09277b44f7d2fa3a142bb50f749791ead7c77
Kim Min Seop <msk7529@naver.com>
2021-05-11 22:44:07 +0900

    백준 1085번(직사각형에서 탈출) - 수학

---
+++ Math/백준1085_직사각형의탈출.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/11.
+//
+/*
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+print(min(min(input[2] - input[0], input[0]), min(input[3] - input[1], input[1])))
+*/
