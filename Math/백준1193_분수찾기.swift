606281765ee3a2d0b745a372e1ec985d9677bc92
Kim Min Seop <msk7529@naver.com>
2021-05-11 22:02:02 +0900

    백준 1193번(분수찾기) - 수학

---
+++ Math/백준1193_분수찾기.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/11.
+//
+/*
+let X: Int = Int(readLine()!)!
+var loopCnt: Int = 1
+var sum: Int = 0
+
+if X == 1 {
+    print("1/1")
+} else {
+    while true {
+        sum += loopCnt
+        if X <= sum {
+            let diff: Int = sum - X
+            let first: Int = loopCnt - diff
+            let second: Int = loopCnt + 1 - first
+            if loopCnt % 2 == 0 {
+                print("\(first)/\(second)")
+            } else {
+                print("\(second)/\(first)")
+            }
+            break
+        }
+        loopCnt += 1
+    }
+}
+*/
