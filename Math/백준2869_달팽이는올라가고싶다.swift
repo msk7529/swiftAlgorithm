6fb1ba86e3aa741bff3585bbdf094ea39e5cd99b
Kim Min Seop <msk7529@naver.com>
2021-05-11 22:21:56 +0900

    백준 2869번(달팽이는 올라가고 싶다) - 수학

---
+++ Math/백준2869_달팽이는올라가고싶다.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/11.
+//
+/*
+import Foundation
+
+var A: Double = 0
+var B: Double = 0
+var V: Double = 0
+
+func input() {
+    let line = readLine()!.split(separator: " ").map { Double(String($0))! }
+    A = line[0]
+    B = line[1]
+    V = line[2]
+    
+    if B == V {
+        print("1")
+    } else {
+        print(Int(ceil((V - B) / (A - B))))
+    }
+}
+
+input()
+*/
