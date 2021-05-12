8fde62ca1fd2aef863f6beda7968fbe24a84f883
Kim Min Seop <msk7529@naver.com>
2021-05-13 00:44:16 +0900

    백준 9461번(파도반 수열) - 수학

---
+++ Math/백준9461_파도반수열.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/12.
+//
+/*
+let T: Int = Int(readLine()!)!
+var P: [Int] = Array(repeating: 1, count: 101)
+
+for i in 4...100 {
+    P[i] = P[i-2] + P[i-3]
+}
+
+for _ in 0..<T {
+    print(P[Int(readLine()!)!])
+}
+*/
+
