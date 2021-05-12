0980cff3799439116cc1e762a283b31ebdd608d4
Kim Min Seop <msk7529@naver.com>
2021-05-13 00:49:32 +0900

    백준 10953번(A+b -6) - 수학

---
+++ Math/백준10953_A+B-6.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/12.
+//
+/*
+let T: Int = Int(readLine()!)!
+
+for _ in 0..<T {
+    let input = readLine()!.split(separator: ",").map { Int(String($0))! }
+    print(input[0] + input[1])
+}
+*/
