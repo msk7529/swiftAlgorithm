b847f553394b8491a0421fa9e281f769056b09a5
Kim Min Seop <msk7529@naver.com>
2021-05-13 00:29:26 +0900

    백준 2581번(소수) - 수학. 에라토스테네스의 체

---
+++ Math/백준2581_소수.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/12.
+//
+/*
+import Foundation
+
+var M: Int = Int(readLine()!)!
+var N: Int = Int(readLine()!)!
+var minPrime: Int = Int.max
+var sum: Int = 0
+
+let primes: [Bool] = getPrimeList(to: N)
+
+for i in M...N {
+    if primes[i] {
+        minPrime = min(i, minPrime)
+        sum += i
+    }
+}
+
+if minPrime == Int.max {
+    print("-1")
+} else {
+    print(sum)
+    print(minPrime)
+}
+*/
--- main.swift
+++ main.swift
@@ -2,5 +2,5 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by MinSeop on 2021/05/11.
+//  Created by MinSeop on 2021/05/12.
 //
