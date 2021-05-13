cd936b8a55aab88f8028bb0304f119064025b680
Kim Min Seop <msk7529@naver.com>
2021-05-14 01:27:12 +0900

    백준 2475번(검증수) - 수학

---
+++ Math/백준2475_검증수.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/13.
+//
+/*
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+print((input[0] * input[0] + input[1] * input[1] + input[2] * input[2] + input[3] * input[3] + input[4] * input[4]) % 10)
+*/
