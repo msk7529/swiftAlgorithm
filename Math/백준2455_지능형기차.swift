da910d35b55657dc9885edecd0b0e2e8d253dd64
Kim Min Seop <msk7529@naver.com>
2021-05-15 01:54:31 +0900

    백준 2455번(지능형 기차) - 수학

---
+++ Math/백준2455_지능형기차.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/15.
+//
+/*
+var result: Int = 0
+var curr: Int = 0
+
+for _ in 0..<4 {
+    let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+    curr = curr - line[0] + line[1]
+    result = max(result, curr)
+}
+print(result)
+*/
