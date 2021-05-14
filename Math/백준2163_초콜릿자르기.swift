c0673829e22a587c62c91b1371c8828fd0da359f
Kim Min Seop <msk7529@naver.com>
2021-05-15 01:22:29 +0900

    백준 2163번(초콜릿 자르기) - 수학

---
+++ Math/백준2163_초콜릿자르기.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/15.
+//
+/*
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+print(input[0] * input[1] - 1)
+*/
