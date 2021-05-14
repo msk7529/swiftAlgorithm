2e5b22690dfbf21f7cffdffe7ef338925ca945d0
Kim Min Seop <msk7529@naver.com>
2021-05-15 02:36:53 +0900

    백준 1037번(약수) - 수학

---
+++ Math/백준1037_약수.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/15.
+//
+/*
+import Foundation
+let N: Int = Int(readLine()!)!
+let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+print(line.min()! * line.max()!)
+*/
