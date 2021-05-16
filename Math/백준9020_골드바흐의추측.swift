fa5ea3e8a26eaea9ad6298de3e61a579fccb14e0
Kim Min Seop <msk7529@naver.com>
2021-05-17 03:27:23 +0900

    백준 9020번(골드바흐의 추측) - 수학

---
+++ Math/백준9020_골드바흐의추측.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/17.
+//
+/*
+import Foundation
+
+let T: Int = Int(readLine()!)!
+var primes: [Bool] = getPrimeList(to: 10000)
+
+for _ in 0..<T {
+    let n: Int = Int(readLine()!)!
+    for num in stride(from: n/2, through: 2, by: -1) {
+        if primes[num] && primes[n - num] {
+            print("\(num) \(n - num)")
+            break
+        }
+    }
+}
+*/
