4ec6d0c717dda21ed8ec73c73d8e84dcee6cf345
Kim Min Seop <msk7529@naver.com>
2021-05-17 02:55:38 +0900

    백준 1476번(날짜 계산) - 수학

---
+++ Math/백준1476_날짜계산.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/17.
+//
+/*
+let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+var num = 1
+
+while true {
+    if (num - line[0]) % 15 == 0 && (num - line[1]) % 28 == 0 && (num - line[2]) % 19 == 0 {
+        print(num)
+        break
+    }
+    num += 1
+}
+*/
