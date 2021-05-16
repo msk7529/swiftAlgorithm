1742cdbc0406708f4c5c655c9b244f093a6abf36
Kim Min Seop <msk7529@naver.com>
2021-05-17 02:05:01 +0900

    백준 3046번(R2) - 수학

---
+++ Math/백준3046_R2.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/16.
+//
+/*
+let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+print(line[1] * 2 - line[0])
+*/
--- main.swift
+++ main.swift
@@ -2,5 +2,5 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by MinSeop on 2021/05/15.
+//  Created by MinSeop on 2021/05/16.
 //
