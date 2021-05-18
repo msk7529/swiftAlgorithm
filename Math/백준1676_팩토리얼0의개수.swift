b828463982be64601adee23d5a4432a5b99c0ea0
Kim Min Seop <msk7529@naver.com>
2021-05-19 03:28:59 +0900

    백준 1676번(팩토리얼 0의 개수) - 수학

---
+++ Math/백준1676_팩토리얼0의개수.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/19.
+//
+/*
+var N: Int = Int(readLine()!)!
+var count: Int = 0
+
+// 5의 배수마다 카운트가 1씩 증가하며, 5의 n승에 따라 카운트값을 한 개 더 추가한다.
+while N >= 5 {
+    count += (N / 5)
+    N /= 5
+}
+
+print(count)
+
+/* 다른 풀이
+print(N / 5 + N / 25 + N / 125)
+*/
+*/
