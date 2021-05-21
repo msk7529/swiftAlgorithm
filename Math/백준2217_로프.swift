cbd95441aac6ae3d46cde1e9261cd482837af46f
Kim Min Seop <msk7529@naver.com>
2021-05-22 02:13:56 +0900

    백준 2217번(로프) - 수학. 그리디

---
+++ Math/백준2217_로프.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/22.
+//
+/*
+let N: Int = Int(readLine()!)!
+var arr: [Int] = []
+var result: Int = 0
+
+for _ in 0..<N {
+    arr.append(Int(readLine()!)!)
+}
+arr.sort()
+
+for i in 0..<N {
+    result = max(result, arr[i] * (N - i))
+}
+print(result)
+*/
