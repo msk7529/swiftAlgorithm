633c528bbe7e37a144f9ad12e5ce2a6214c8b0ed
Kim Min Seop <msk7529@naver.com>
2021-05-23 22:23:43 +0900

    백준 1977번(완전제곱수) - 수학

---
+++ Math/백준1977_완전제곱수.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/23.
+//
+/*
+import Foundation
+
+var powArr: [Int] = []
+let M = Int(ceil(sqrt(Double(readLine()!)!)))
+let N = Int(sqrt(Double(readLine()!)!))
+
+for i in stride(from: M, through: N, by: 1) {
+    powArr.append(i * i)
+}
+
+if powArr.isEmpty {
+    print("-1")
+} else {
+    print(powArr.reduce(0,+))
+    print(powArr.min()!)
+}
+*/
