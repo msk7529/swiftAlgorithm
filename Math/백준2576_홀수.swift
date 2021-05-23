705d34824be8a0467b4c8333e0b1cf295b7a87b7
Kim Min Seop <msk7529@naver.com>
2021-05-23 22:03:30 +0900

    백준 2576번(홀수) - 수학

---
+++ Math/백준2576_홀수.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/23.
+//
+/*
+var arr: [Int] = []
+var minNum: Int = Int.max
+
+for _ in 0..<7 {
+    let num = Int(readLine()!)!
+    if num % 2 == 1 {
+        arr.append(num)
+        minNum = min(minNum, num)
+    }
+}
+
+if arr.isEmpty {
+    print("-1")
+} else {
+    print(arr.reduce(0,+))
+    print(minNum)
+}
+*/
