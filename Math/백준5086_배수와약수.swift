fbebb4d4826286af4cb54f66beddc24a62ba5380
Kim Min Seop <msk7529@naver.com>
2021-05-23 22:28:55 +0900

    백준 5086번(배수와 약수) - 수학

---
+++ Math/백준5086_배수와약수.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/23.
+//
+/*
+while true {
+    let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+    
+    if line[0] == 0 && line[1] == 0 {
+        break
+    }
+    
+    if line[0] % line[1] == 0 {
+        print("multiple")
+    } else if line[1] % line[0] == 0 {
+        print("factor")
+    } else {
+        print("neither")
+    }
+}
+*/
