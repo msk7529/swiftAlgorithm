8a9400e9275376cfa055d0cd40642ce81c4bc29c
Kim Min Seop <msk7529@naver.com>
2021-05-23 22:12:34 +0900

    백준 6588번(골드바흐의 추측) - 수학

---
+++ Math/백준6588_골드바흐의추측.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/23.
+//
+/*
+import Foundation
+
+let primes = getPrimeList(to: 1000000)
+
+while true {
+    let num = Int(readLine()!)!
+    
+    if num == 0 { break }
+    
+    for i in 3..<num {
+        if primes[i] && primes[num - i] {
+            print("\(num) = \(i) + \(num - i)")
+            break
+        }
+    }
+}
+*/
