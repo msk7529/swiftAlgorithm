baa35e5e8be6f93aa38a54497c4b5e44f82f4d31
Kim Min Seop <msk7529@naver.com>
2021-05-24 01:43:02 +0900

    백준 1212번(8진수 2진수) - 수학. String append가 매우 많이 발생하거나, Int <-> String 변환이 매우 많이 발생하면 시간초과에 걸림.

---
+++ Math/백준1212_8진수2진수.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/24.
+//
+/*
+let num = readLine()!.map { Int(String($0))! }
+
+if num.count == 1 && num.first! == 0 {
+    print("0")
+} else {
+    for (index, curr) in num.enumerated() {
+        if index == 0 {
+            print(String(curr, radix: 2), terminator: "")
+        } else {
+            var currNum = String(curr, radix: 2)
+            
+            while currNum.count != 3 {
+                currNum = "0" + currNum
+            }
+            print(currNum, terminator: "")
+        }
+    }
+}
+*/
