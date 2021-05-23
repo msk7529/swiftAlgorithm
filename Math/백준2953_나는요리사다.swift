079ff5f52fdc0a488f598c90838bf98e728437ea
Kim Min Seop <msk7529@naver.com>
2021-05-24 00:08:32 +0900

    백준 2953번(나는 요리사다) - 수학

---
+++ Math/백준2953_나는요리사다.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/23.
+//
+/*
+var resultNum: Int = 0
+var resultSum: Int = 0
+
+for i in 1...5 {
+    let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+    let sum = line.reduce(0, +)
+    
+    if sum > resultSum {
+        resultSum = sum
+        resultNum = i
+    }
+}
+
+print("\(resultNum) \(resultSum)")
+*/
