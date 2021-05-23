404cf0632c767530da7bbf9a2cfbcfd2b2c244fe
Kim Min Seop <msk7529@naver.com>
2021-05-24 00:43:22 +0900

    백준 2875번(대회 or 인턴) - 수학. 그리디

---
+++ Math/백준2875_대회or인턴.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/23.
+//
+/*
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+var (N, M, K) = (input[0], input[1], input[2])
+var result: Int = 0
+
+while N >= 2 && M >= 1 {
+    N -= 2
+    M -= 1
+    result += 1
+}
+
+while N + M < K {
+    N += 2
+    M += 1
+    result -= 1
+}
+
+print(result)
+*/
