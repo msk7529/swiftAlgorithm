42926fc778906c89172fd8f2c3d6fa9af820635f
Kim Min Seop <msk7529@naver.com>
2021-05-26 01:55:34 +0900

    백준 2010번(플러그) - 수학

---
+++ Math/백준2010_플러그.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/26.
+//
+/*
+import Foundation
+
+let file: FileIO = .init()
+let N: Int = file.readInt()
+var sum: Int = 0
+
+for _ in 0..<N {
+    sum += file.readInt()
+}
+
+print(sum - (N - 1))
+*/
