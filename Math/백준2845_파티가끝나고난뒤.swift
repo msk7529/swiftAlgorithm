f2b616d3799bffd51fd236b8a7a693ab1cef78bc
Kim Min Seop <msk7529@naver.com>
2021-05-27 01:38:01 +0900

    백준 2845번(파티가 끝나고 난 뒤) - 수학

---
+++ Math/백준2845_파티가끝나고난뒤.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/27.
+//
+/*
+let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+let total = line[0] * line[1]
+let newspaper = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+for num in newspaper {
+    print(num - total, terminator: " ")
+}
+*/
