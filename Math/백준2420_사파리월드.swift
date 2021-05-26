93151a55ca1117c77fa9a61dfc83f819293dd8a6
Kim Min Seop <msk7529@naver.com>
2021-05-27 02:02:50 +0900

    백준 2420번(사파리월드) - 수학

---
+++ Math/백준2420_사파리월드.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/27.
+//
+/*
+let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+print(abs(line[0] - line[1]))
+*/
