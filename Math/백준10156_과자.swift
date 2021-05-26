b2f63f9e257df12960e0f197c6f23fcd4bc5e82e
Kim Min Seop <msk7529@naver.com>
2021-05-27 01:32:08 +0900

    백준 10156번(과자) - 수학

---
+++ Math/백준10156_과자.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/27.
+//
+/*
+let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+let total: Int = line[0] * line[1]
+
+if total > line[2] {
+    print(total - line[2])
+} else {
+    print("0")
+}
+*/
