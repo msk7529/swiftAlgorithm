f31d888da775b1d8cc880031c159d07b23ee2908
Kim Min Seop <msk7529@naver.com>
2021-05-27 01:23:03 +0900

    백준 10162번(전자레인지) - 수학

---
+++ Math/백준10162_전자레인지.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/05/27.
+//
+/*
+var T: Int = Int(readLine()!)!
+var (a, b, c) = (0, 0, 0)
+
+if T % 10 != 0 {
+    print("-1")
+} else {
+    a = T / 300
+    T -= a * 300
+    
+    b = T / 60
+    T -= b * 60
+    
+    c = T / 10
+    T -= c * 10
+    
+    print("\(a) \(b) \(c)")
+}
+*/
