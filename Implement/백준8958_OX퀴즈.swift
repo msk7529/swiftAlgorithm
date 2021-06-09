ab89068301dd7dfde2f08e4f21f950a2d058f4e8
Kim Min Seop <msk7529@naver.com>
2021-06-09 23:16:25 +0900

    백준 8958번(OX퀴즈) - 구현

---
+++ Implement/백준8958_OX퀴즈.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/06/09.
+//
+/*
+let T: Int = Int(readLine()!)!
+
+for _ in 0..<T {
+    let line = readLine()!
+    var successScore: Int = 0
+    var score: Int = 0
+
+    for curr in line {
+        if curr == "O" {
+            successScore += 1
+            score += successScore
+        } else {
+            successScore = 0
+        }
+    }
+
+    print(score)
+}
+*/
