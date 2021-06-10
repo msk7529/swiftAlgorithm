f044c99be2351e8ca06c22f94ca6c9e07835b447
Kim Min Seop <msk7529@naver.com>
2021-06-10 23:28:15 +0900

    백준 2675번(문자열 반복) - 구현

---
+++ Implement/백준2675_문자열반복.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/06/09.
+//
+/*
+let T: Int = Int(readLine()!)!
+for _ in 0..<T {
+    let line = readLine()!.split(separator: " ").map { String($0) }
+    let repeatCount: Int = Int(line[0])!
+    for curr in line[1] {
+        for _ in 0..<repeatCount {
+            print(curr, terminator: "")
+        }
+    }
+    print("")
+}
+*/
--- main.swift
+++ main.swift
@@ -2,5 +2,5 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by MinSeop on 2021/06/09.
+//  Created by MinSeop on 2021/06/10.
 //
