73332f40b1466f9a7250c4fd2c81b4f695a2c02c
Kim Min Seop <msk7529@naver.com>
2021-06-23 13:32:37 +0900

    백준 10773번(제로) - 구현

---
+++ Implement/백준10773_제로.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/06/23.
+//
+/*
+let K: Int = Int(readLine()!)!
+var arr: [Int] = []
+
+for _ in 0..<K {
+    let num: Int = Int(readLine()!)!
+    if num != 0 {
+        arr.append(num)
+    } else {
+        arr.removeLast()
+    }
+}
+
+print(arr.reduce(0,+))
+*/
