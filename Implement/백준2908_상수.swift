bd2400fdc03c31e91c3a12637a1a50ae78caecfd
Kim Min Seop <msk7529@naver.com>
2021-06-25 23:21:58 +0900

    백준 2908번(상수) - 구현

---
+++ Implement/백준2908_상수.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/06/25.
+//
+/*
+let input = readLine()!.split(separator: " ").map { $0.reversed() }.map { Int(String($0))! }
+print(max(input[0], input[1]))
+*/
