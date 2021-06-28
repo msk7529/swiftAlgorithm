99c00c3ba15f3949f9af70d727604c0f461432cf
Kim Min Seop <msk7529@naver.com>
2021-06-28 23:49:38 +0900

    백준 3009번(네 번째 점) - 구현

---
+++ Implement/백준3009_네번째점.swift
@@ -0,0 +1,42 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/06/28.
+//
+/*
+var yDic: [Int: Int] = [:]
+var xDic: [Int: Int] = [:]
+var resultY: Int = 0
+var resultX: Int = 0
+
+for _ in 0..<3 {
+    let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+    
+    if let value = yDic[line[0]] {
+        yDic[line[0]] = value + 1
+    } else {
+        yDic[line[0]] = 1
+    }
+    
+    if let value = xDic[line[1]] {
+        xDic[line[1]] = value + 1
+    } else {
+        xDic[line[1]] = 1
+    }
+}
+
+for curr in yDic {
+    if curr.value == 1 {
+        resultY = curr.key
+    }
+}
+
+for curr in xDic {
+    if curr.value == 1 {
+        resultX = curr.key
+    }
+}
+
+print("\(resultY) \(resultX)")
+*/
