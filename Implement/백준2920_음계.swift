a0e85e5fd5b4f52c51eb4324c9bc828adec3a157
Kim Min Seop <msk7529@naver.com>
2021-07-16 04:09:43 +0900

    백준 2920번(음계) - 구현

---
+++ Implement/백준2920_음계.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/07/16.
+//
+/*
+let numbers: String = "12345678"
+let input: String = readLine()!.split(separator: " ").joined()
+
+if numbers == input {
+    print("ascending")
+} else if String(numbers.reversed()) == input {
+    print("descending")
+} else {
+    print("mixed")
+}
+*/
