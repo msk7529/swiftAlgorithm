e566bdeedc46096629976ae1cac6678a9015fe96
Kim Min Seop <msk7529@naver.com>
2021-07-19 02:29:05 +0900

    프로그래머스 2021 카카오 채용연계형 인턴십 1번(숫자 문자열과 영단어) - 구현

---
+++ Implement/프로그래머스_숫자문자열과영단어.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/07/19.
+//
+/*
+var result: String = ""
+
+func solution(_ s: String) -> Int {
+    let numbers: [String] = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    
+    var tmp: String = ""
+    for curr in s {
+        if curr.isNumber {
+            result.append(String(curr))
+        } else {
+            tmp.append(String(curr))
+            
+            if let index = numbers.firstIndex(of: tmp) {
+                result.append(String(index))
+                tmp = ""
+            }
+        }
+    }
+    
+    return Int(result)!
+}
+*/
