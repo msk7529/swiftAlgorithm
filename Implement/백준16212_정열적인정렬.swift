b520f3008133b990787aa892dd4f97818f5b1962
Kim Min Seop <msk7529@naver.com>
2021-07-23 03:04:17 +0900

    백준 16212번(정렬적인 정렬) - 구현

---
+++ Implement/백준16212_정열적인정렬.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/07/23.
+//
+/*
+let N: Int = Int(readLine()!)!
+let arr: [Int] = readLine()!.split(separator: " ").map { Int(String($0))! }.sorted()
+var result: String = ""
+for curr in arr {
+    result += "\(curr) "
+}
+print(result)
+*/
