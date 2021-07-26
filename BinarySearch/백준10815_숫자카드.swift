8ab465051c2e4deaa8cc4afed02990131222da68
Kim Min Seop <msk7529@naver.com>
2021-07-26 10:14:16 +0900

    백준 10815번(숫자 카드) - 이분탐색. Set을 이용하는거랑 binarySearch를 이용하는거랑 별 차이는 없는듯. Set가 16ms 빨랐음

---
+++ BinarySearch/백준10815_숫자카드.swift
@@ -0,0 +1,40 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/07/26.
+//
+/*
+let N: Int = Int(readLine()!)!
+let arr: [Int] = readLine()!.split(separator: " ").map { Int(String($0))! }.sorted()
+let M: Int = Int(readLine()!)!
+let arr2: [Int] = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+for num in arr2 {
+    if arr.binarySearchForAscending(value: num) != -1 {
+        print("1")
+    } else {
+        print("0")
+    }
+}
+*/
+
+/*
+ let N: Int = Int(readLine()!)!
+ let arr: [Int] = readLine()!.split(separator: " ").map { Int(String($0))! }
+ let M: Int = Int(readLine()!)!
+ let arr2: [Int] = readLine()!.split(separator: " ").map { Int(String($0))! }
+ var set: Set<Int> = .init()
+
+ for num in arr {
+     set.insert(num)
+ }
+
+ for num in arr2 {
+     if set.contains(num) {
+         print("1")
+     } else {
+         print("0")
+     }
+ }
+*/
