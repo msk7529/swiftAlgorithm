2713c659a3e445e3cda072b5f6b6c99cdb5d8e79
Kim Min Seop <msk7529@naver.com>
2021-07-30 00:27:13 +0900

    백준 1100번(하얀칸) - 구현

---
+++ Implement/백준1100_하얀칸.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/07/30.
+//
+/*
+var result: Int = 0
+
+for i in 0..<8 {
+    let line = readLine()!
+    let firstIsWhite = i.isMultiple(of: 2)
+    
+    for (index, value) in line.enumerated() {
+        if firstIsWhite && index.isMultiple(of: 2) && value == "F" {
+            result += 1
+        } else if !firstIsWhite && !index.isMultiple(of: 2) && value == "F" {
+            result += 1
+        }
+    }
+}
+
+print(result)
+*/
