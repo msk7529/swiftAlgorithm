aaa9678b0e5ab4c6abd2a0fa772ac94494b6ffdf
Kim Min Seop <msk7529@naver.com>
2021-07-30 00:01:02 +0900

    백준 8595번(히든 넘버) - 구현

---
+++ Implement/백준8595_히든넘버.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/07/30.
+//
+/*
+let N: Int = Int(readLine()!)!
+let word = readLine()! + "a"
+var arr: [String] = []
+var result: Int = 0
+var tmp: Int = 0
+
+for curr in word {
+    if curr.isNumber {
+        let toNum = Int(curr.asciiValue! - 48)
+        if tmp == 0 {
+            tmp = toNum
+        } else {
+            tmp = tmp * 10 + toNum
+        }
+    } else {
+        if tmp != 0 {
+            result += tmp
+            tmp = 0
+        }
+    }
+}
+
+print(result)
+*/
