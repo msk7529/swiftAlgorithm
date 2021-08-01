b219568d6f914d1e5d4235b4c93faf77b4440124
Kim Min Seop <msk7529@naver.com>
2021-08-02 01:40:28 +0900

    백준 11656번(접미사 배열) - 구현

---
+++ Implement/백준11656_접미사배열.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/02.
+//
+/*
+var word = readLine()!
+var result: [String] = []
+
+while !word.isEmpty {
+    result.append(word)
+    _ = word.removeFirst()
+}
+
+result.sort()
+
+print(result.joined(separator: "\n"))
+*/
