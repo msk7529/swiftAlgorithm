4116aceccf89bcb9caccddcacb31d66c9fb0cdfa
Kim Min Seop <msk7529@naver.com>
2021-08-04 02:26:11 +0900

    백준 2776번(암기왕) - 구현

---
+++ Implement/백준2776_암기왕.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/04.
+//
+/*
+let T = Int(readLine()!)!
+for _ in 0..<T {
+    let _ = Int(readLine()!)!
+    let set = Set(readLine()!.split(separator: " ").map { Int(String($0))! })
+    let _ = Int(readLine()!)!
+    let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+    var result = ""
+    
+    for curr in arr {
+        if set.contains(curr) {
+            result += "1\n"
+        } else {
+            result += "0\n"
+        }
+    }
+    _ = result.removeLast()
+    print(result)
+}
+*/
