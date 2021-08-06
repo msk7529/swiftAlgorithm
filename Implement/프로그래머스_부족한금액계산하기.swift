3e08fa677535e8667d90e9c65c677d2246564ccc
Kim Min Seop <msk7529@naver.com>
2021-08-07 03:34:58 +0900

    프로그래머스 8월 1주차 위클리 챌린지(부족한 금액 계산하기) - 구현

---
+++ Implement/프로그래머스_부족한금액계산하기.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/07.
+//
+/*
+func solution(_ price: Int, _ money: Int, _ count: Int) -> Int {
+    var nextPrice = price
+    var result = 0
+    
+    for _ in 0..<count {
+        result += nextPrice
+        nextPrice += price
+    }
+    
+    return money - result >= 0 ? 0 : result - money
+}
+
+//print(solution(3, 20, 4))
+*/
