8fc5ae52b1438ed1df7b19a013aaeba127a8a512
Kim Min Seop <msk7529@naver.com>
2021-08-08 01:01:53 +0900

    백준 1436번(영화감독 숌) - 구현

---
+++ Implement/백준1436_영화감독숌.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/08.
+//
+/*
+let N = Int(readLine()!)!
+var curr = 665
+var tmp = 0
+var count = 0
+
+while count != N {
+    curr += 1
+    tmp = curr
+    
+    while tmp != 0 {
+        if tmp % 1000 == 666 {
+            count += 1
+            break
+        } else {
+            tmp /= 10   // 일의 자리수를 삭제
+        }
+    }
+}
+
+print(curr)
+*/
--- main.swift
+++ main.swift
@@ -2,5 +2,5 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by MinSeop on 2021/08/07.
+//  Created by MinSeop on 2021/08/08.
 //
