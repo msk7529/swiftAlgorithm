0bea009cad6835ece07f2324e0e69aa92f8058ae
Kim Min Seop <msk7529@naver.com>
2021-08-09 01:53:51 +0900

    백준 10868번(최솟값) - 세그먼트트리

---
+++ SegmentTree/백준10868_최솟값.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/09.
+//
+/*
+let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+let N = line[0], M = line[1]
+var arr: [Int] = Array(repeating: 0, count: N)
+var result: String = ""
+
+for i in 0..<N {
+    arr[i] = Int(readLine()!)!
+}
+
+let segmentTree: SegmentTree<Int> = .init(array: arr, function: { a, b in a < b ? a : b })
+
+for _ in 0..<M {
+    let query = readLine()!.split(separator: " ").map { Int(String($0))! }
+    result += "\(segmentTree.query(leftBound: query[0] - 1, rightBound: query[1] - 1))\n"
+}
+
+result.removeLast()
+print(result)
+*/
