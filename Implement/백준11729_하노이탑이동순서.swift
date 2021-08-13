dfd60c15ec80d412f4f86bb493b579ad1b9c8ffb
Kim Min Seop <msk7529@naver.com>
2021-08-14 08:54:57 +0900

    백준 11729번(하노이 탑 이동 순서) - 구현. 재귀

---
+++ Implement/백준11729_하노이탑이동순서.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/14.
+//
+/*
+func hanoi(n: Int, start: Int, middle: Int, end: Int) {
+    if n == 1 {
+        result += "\(start) \(end)\n"
+        count += 1
+        return
+    }
+    
+    hanoi(n: n - 1, start: start, middle: end, end: middle)
+    hanoi(n: 1, start: start, middle: middle, end: end)
+    hanoi(n: n - 1, start: middle, middle: start, end: end)
+}
+
+let N = Int(readLine()!)!
+var result = ""
+var count = 0
+
+hanoi(n: N, start: 1, middle: 2, end: 3)
+print(count)
+print(result)
+*/
