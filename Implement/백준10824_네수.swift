3614eb540aebbd22475d1938d07db9355603e78a
Kim Min Seop <msk7529@naver.com>
2021-08-14 02:48:15 +0900

    백준 10824번(네 수) - 구현

---
+++ Implement/백준10824_네수.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/14.
+//
+/*
+let line = readLine()!.split(separator: " ")
+print(Int(line[0] + line[1])! + Int(line[2] + line[3])!)
+*/
