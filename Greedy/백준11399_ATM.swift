2f2f55b5d4bb17b761650bbb3825adb46c7ea7b2
Kim Min Seop <msk7529@naver.com>
2021-08-15 03:31:00 +0900

    백준 11399번(ATM) - 그리디

---
+++ Greedy/백준11399_ATM.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/15.
+//
+/*
+let N = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map { Int(String($0))! }.sorted()
+print(arr.reduce((0, 0)) { result, curr in (result.0 + result.1 + curr, result.1 + curr) }.0)
+*/
