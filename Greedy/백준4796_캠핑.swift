888c4cc89ccbf9dc264a06bd3bbbd0fad0ce52e5
Kim Min Seop <msk7529@naver.com>
2021-08-15 08:45:59 +0900

    백준 4796번(캠핑) - 그리디

---
+++ Greedy/백준4796_캠핑.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/15.
+//
+/*
+var tc: Int = 1
+
+while true {
+    let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+    if line[0] == 0 { break }
+    let L = line[0], P = line[1], V = line[2]
+    
+    let q = V / P
+    print("Case \(tc): \(L * q + min(L, V - q * P))")
+    
+    tc += 1
+}
+*/
