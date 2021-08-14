79a359617cc037035ffde4977dc4f08216c2662d
Kim Min Seop <msk7529@naver.com>
2021-08-15 00:15:20 +0900

    백준 10989번(수 정렬하기 3) - 구현. Counting sort

---
+++ Implement/백준10989_수정렬하기3.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/15.
+//
+/*
+import Foundation
+
+let file: FileIO = .init()
+let N = file.readInt()
+var arr: [Int] = Array(repeating: 0, count: 10001)
+var result: String = ""
+
+for _ in 0..<N {
+    arr[file.readInt()] += 1
+}
+
+for i in 1...10000 {
+    if arr[i] > 0 {
+        result += String(repeating: "\(i)\n", count: arr[i])
+    }
+}
+
+result.removeLast()
+print(result)
+*/
