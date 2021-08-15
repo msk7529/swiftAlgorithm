0f75cc46a78c208a6dc26d8c9191122247069de9
Kim Min Seop <msk7529@naver.com>
2021-08-16 05:26:37 +0900

    백준 1543번(문서 검색) - 그리디. 구현

---
+++ Greedy/백준1543_문서검색.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/16.
+//
+/*
+let doc = readLine()!.map { $0 }
+let word = readLine()!.map { $0 }
+var index: Int = 0
+var result = 0
+
+while index + word.count <= doc.count {
+    if Array(doc[index..<index+word.count]) == word {
+        result += 1
+        index += word.count
+    } else {
+        index += 1
+    }
+}
+
+print(result)
+*/
