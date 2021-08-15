1080ff74d6b868ea9f893304dbfa37c5aa48e714
Kim Min Seop <msk7529@naver.com>
2021-08-16 06:26:22 +0900

    백준 1783번(병든 나이트) - 그리디. 구현

---
+++ Greedy/백준1783_병든나이트.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/16.
+//
+/*
+let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+let N = line[0], M = line[1]
+
+if N == 1 {
+    print("1")
+} else if N == 2 {
+    print(min(4, (M - 1) / 2 + 1))
+} else {
+    if M >= 7 {
+        print(M - 2)
+    } else {
+        print(min(4, M))
+    }
+}
+*/
