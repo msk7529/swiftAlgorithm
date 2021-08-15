685ea8eb89da4cdb04a8ff8155122a85534c48d4
Kim Min Seop <msk7529@naver.com>
2021-08-15 09:51:47 +0900

    백준 1439번(뒤집기) - 그리디

---
+++ Greedy/백준1439_뒤집기.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/15.
+//
+/*
+let line = readLine()!
+print(min(line.split(separator: "0").count, line.split(separator: "1").count))
+*/
