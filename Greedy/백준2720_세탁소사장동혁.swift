e50d4865768867c6d51d5606ba2ad4f846425280
Kim Min Seop <msk7529@naver.com>
2021-08-17 10:17:07 +0900

    백준 2720번(세탁소 사장 동혁) - 그리디. 구현

---
+++ Greedy/백준2720_세탁소사장동혁.swift
@@ -0,0 +1,33 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/17.
+//
+/*
+for _ in 0..<Int(readLine()!)! {
+    var C = Int(readLine()!)!
+    var a = 0, b = 0, c = 0, d = 0
+    
+    if C / 25 != 0 {
+        a = C / 25
+        C -= a * 25
+    }
+    
+    if C / 10 != 0 {
+        b = C / 10
+        C -= b * 10
+    }
+    
+    if C / 5 != 0 {
+        c = C / 5
+        C -= c * 5
+    }
+    
+    if C != 0 {
+        d = C
+    }
+    
+    print("\(a) \(b) \(c) \(d)")
+}
+*/
