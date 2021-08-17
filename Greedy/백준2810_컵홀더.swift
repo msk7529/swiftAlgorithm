9bc8c7b8512a0d6d62413fd622e0579d3191d919
Kim Min Seop <msk7529@naver.com>
2021-08-18 04:22:36 +0900

    백준 2810번(컵홀더) - 그리디

---
+++ Greedy/백준2810_컵홀더.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/18.
+//
+/*
+let N = Int(readLine()!)!
+var str = readLine()!.map { $0 }
+var result = 0
+
+while !str.isEmpty {
+    if str.last! == "L" {
+        str.removeLast()
+    }
+    str.removeLast()
+    result += 1
+}
+
+print(min(result + 1, N))
+*/
