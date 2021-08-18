19e280950078919b1745d407b621c5b9e78b4ba9
Kim Min Seop <msk7529@naver.com>
2021-08-19 04:40:50 +0900

    백준 11655번(ROT13) - 구현. 문자열

---
+++ Implement/백준11655_ROT13.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/19.
+//
+/*
+let str = readLine()!
+var result: String = ""
+
+str.forEach {
+    if $0.isUppercase {
+        var ascii = $0.asciiValue! + 13
+        ascii = ascii > 90 ? ascii - 26 : ascii
+        result += String(UnicodeScalar(ascii))
+    } else if $0.isLowercase {
+        var ascii = $0.asciiValue! + 13
+        ascii = ascii > 122 ? ascii - 26 : ascii
+        result += String(UnicodeScalar(ascii))
+    } else {
+        result += String($0)
+    }
+}
+
+print(result)
+*/
--- main.swift
+++ main.swift
@@ -2,5 +2,5 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by MinSeop on 2021/08/18.
+//  Created by MinSeop on 2021/08/19.
 //
