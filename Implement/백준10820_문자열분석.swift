97d20b6613b564886818da396754f244134536ed
Kim Min Seop <msk7529@naver.com>
2021-08-20 18:45:03 +0900

    백준 10820번(문자열 분석) - 구현. 문자열

---
+++ Implement/백준10820_문자열분석.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/20.
+//
+/*
+while true {
+    guard let line = readLine() else { break }
+    
+    var a = 0, b = 0, c = 0, d = 0
+    
+    line.forEach {
+        if $0.isUppercase {
+            b += 1
+        } else if $0.isLowercase {
+            a += 1
+        } else if $0.isNumber {
+            c += 1
+        } else if $0 == " " {
+            d += 1
+        }
+    }
+    print("\(a) \(b) \(c) \(d)")
+}
+*/
