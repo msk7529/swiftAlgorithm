d4db61c3a2e2a31d1bf7e0b7e75585dfc9bd48a5
Kim Min Seop <msk7529@naver.com>
2021-08-21 20:29:36 +0900

    백준 7567번(그릇) - 구현. 문자열

---
+++ Implement/백준7567_그릇.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/21.
+//
+/*
+let line = Array(readLine()!)
+var result = 10
+var prev: Character = line.first!
+
+for i in stride(from: 1, to: line.count, by: 1) {
+    if line[i] == prev {
+        result += 5
+    } else {
+        result += 10
+        prev = line[i]
+    }
+}
+print(result)
+*/
