1a338bf76146570ecf30ecc65d7e861f919a110e
Kim Min Seop <msk7529@naver.com>
2021-08-22 19:45:31 +0900

    백준 9093번(단어 뒤집기) - 구현. 문자열

---
+++ Implement/백준9093_단어뒤집기.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/22.
+//
+/*
+for _ in 0..<Int(readLine()!)! {
+    print(readLine()!.split(separator: " ").map{ String($0.reversed()) }.joined(separator: " "))
+}
+*/
