e42c117981dab630561c8990dc7a9c4a4e3149d0
Kim Min Seop <msk7529@naver.com>
2021-08-22 22:58:01 +0900

    백준 1373번(2진수 8진수) - 구현. 문자열

---
+++ Implement/백준1373_2진수8진수.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/22.
+//
+/*
+var num = Array(readLine()!.map { String($0) })
+var index = 0
+var result: String = ""
+
+while num.count % 3 != 0 {
+    num.insert("0", at: 0)
+}
+
+while index < num.count {
+    let str = num[index..<index+3].joined()
+    let strToDecimal = Int(str, radix: 2)!
+    result.append(String(strToDecimal, radix: 8, uppercase: false))
+    index += 3
+}
+
+print(result)
+*/
