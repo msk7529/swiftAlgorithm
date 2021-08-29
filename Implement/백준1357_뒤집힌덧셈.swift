ee456051f19aa9e5f749a557ffb3dcdef39b8ffe
Kim Min Seop <msk7529@naver.com>
2021-08-30 02:18:56 +0900

    백준 1357번(뒤집힌 덧셈) - 구현. 문자열

---
+++ Implement/백준1357_뒤집힌덧셈.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/30.
+//
+/*
+let line = readLine()!.split(separator: " ").map { String($0) }
+let num1 = String(line[0].reversed())
+let num2 = String(line[1].reversed())
+let sum = Int(num1)! + Int(num2)!
+var result = String(String(sum).reversed())
+while result.first! == "0" {
+    result.removeFirst()
+}
+print(result)
+*/
