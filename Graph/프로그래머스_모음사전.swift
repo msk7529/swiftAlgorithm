49f5922f9f63d22d1bd3882c21b24ddfb233ceb2
Kim Min Seop <msk7529@naver.com>
2021-08-30 14:18:44 +0900

    프로그래머스 8월 5주차 위클리 챌린지(모음 사전) - DFS

---
+++ Graph/프로그래머스_모음사전.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/08/30.
+//
+/*
+func solution(_ word: String) -> Int {
+    func dfs(curr: String) {
+        result.append(curr)
+        
+        if curr.count == 5 { return }
+        
+        for i in 0..<5 {
+            dfs(curr: curr + alphabet[i])
+        }
+    }
+    
+    let alphabet: [String] = ["A", "E", "I", "O", "U"]
+    var result: [String] = []
+    
+    dfs(curr: "")
+    
+    return result.firstIndex(of: word)!
+}
+//print(solution("AAAAE"))
+//print(solution("AAAE"))
+//print(solution("I"))
+//print(solution("EIO"))
+*/
