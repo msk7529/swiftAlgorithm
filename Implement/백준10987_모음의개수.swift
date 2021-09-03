f1254555451d89886e17e50682a0ae88acf286a6
Kim Min Seop <msk7529@naver.com>
2021-09-03 22:36:26 +0900

    백준 10987번(모음의 개수) - 구현. 문자열

---
+++ Implement/백준10987_모음의개수.swift
@@ -0,0 +1,7 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/09/03.
+//
+//print(readLine()!.filter { ["a", "e", "i", "o", "u"].contains($0) }.count)
--- main.swift
+++ main.swift
@@ -2,5 +2,5 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by MinSeop on 2021/09/02.
+//  Created by MinSeop on 2021/09/03.
 //
