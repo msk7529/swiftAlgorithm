907d6a591c2eb8725405a2369b038be9c881b374
Kim Min Seop <msk7529@naver.com>
2021-09-18 23:44:54 +0900

    백준 9086번(문자열) - 문자열. 구현

---
+++ Implement/백준9086_문자열.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/09/18.
+//
+/*
+let T = Int(readLine()!)!
+for _ in 0..<T {
+    let word = readLine()!
+    print("\(word.first!)\(word.last!)")
+}
+*/
--- main.swift
+++ main.swift
@@ -2,5 +2,5 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by MinSeop on 2021/09/17.
+//  Created by MinSeop on 2021/09/18.
 //
