d83841484cc8762891e1cb24f76b914339f039d7
Kim Min Seop <msk7529@naver.com>
2021-09-26 05:18:45 +0900

    백준 1026번(보물) - 수학. 정렬

---
+++ Math/백준1026_보물.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/09/26.
+//
+/*
+let N = Int(readLine()!)!
+var A = readLine()!.split(separator: " ").map { Int(String($0))! }.sorted { $0 < $1 }
+var B = readLine()!.split(separator: " ").map { Int(String($0))! }.sorted { $0 > $1 }
+
+print(A.enumerated().reduce(0) { totalSum, curr in
+    return totalSum + curr.element * B[curr.offset]
+})
+*/
