1d5d37e1d9aa79c5ff5a45b9a13563745ec0bb97
Kim Min Seop <msk7529@naver.com>
2021-10-01 03:05:18 +0900

    백준 5338번(마이크로소프트 로고) - 구현. 문자열 출력

---
+++ Implement/백준5338_마이크로소프트로고.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/01.
+//
+/*
+let str: String = """
+       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|
+"""
+print(str)
+*/
