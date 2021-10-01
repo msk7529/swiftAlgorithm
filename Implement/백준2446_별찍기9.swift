ce84c105543143cbc704f054f7879f324a044074
Kim Min Seop <msk7529@naver.com>
2021-10-02 03:31:48 +0900

    백준 2446번(별 찍기 - 9) - 구현

---
+++ Implement/백준2446_별찍기9.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/02.
+//
+/*
+let N = Int(readLine()!)!
+for i in 1...N {
+    var str = String(repeating: "*", count: 2 * (N - i) + 1)
+    let emptyStr = String(repeating: " ", count: (2 * N - 1 - str.count) / 2)
+    str = emptyStr + str
+    print(str)
+}
+for i in stride(from: 2, through: N, by: 1) {
+    var str = String(repeating: "*", count: 2 * (i - 1) + 1)
+    let emptyStr = String(repeating: " ", count: (2 * N - 1 - str.count) / 2)
+    str = emptyStr + str
+    print(str)
+}
+*/
