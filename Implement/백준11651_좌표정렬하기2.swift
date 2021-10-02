767b40ff0af1bfe9029076c0c7e910325e06f936
Kim Min Seop <msk7529@naver.com>
2021-10-03 02:42:52 +0900

    백준 11651번(좌표 정렬하기 2) - 구현. 정렬

---
+++ Implement/백준11651_좌표정렬하기2.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/03.
+//
+/*
+let N = Int(readLine()!)!
+var arr: [(Int, Int)] = []
+var result: String = ""
+
+for _ in 0..<N {
+    let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+    arr.append((line[0], line[1]))
+}
+
+arr.sort(by: {
+    if $0.1 == $1.1 {
+        return $0.0 < $1.0
+    }
+    return $0.1 < $1.1
+})
+
+for curr in arr {
+    result += "\(curr.0) \(curr.1)\n"
+}
+result.removeLast()
+print(result)
+*/
