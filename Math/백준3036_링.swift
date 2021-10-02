5a8f0b1f860abc3db947c540a3abc1cf36c07e24
Kim Min Seop <msk7529@naver.com>
2021-10-03 02:07:09 +0900

    백준 3036번(링) - 수학

---
+++ Math/백준3036_링.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/03.
+//
+/*
+let N = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+var result: String = ""
+
+for i in 1..<N {
+    let g = gcd(arr[0], arr[i])
+    result += "\(arr[0] / g)/\(arr[i] / g)\n"
+}
+result.removeLast()
+print(result)
+*/
