88861e44d4926fdd2be473ea19063b16a2ca78f3
Kim Min Seop <msk7529@naver.com>
2021-10-03 02:49:31 +0900

    백준 1181번(단어 정렬) - 구현. 정렬

---
+++ Implement/백준1181_단어정렬.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/03.
+//
+/*
+let N = Int(readLine()!)!
+var set: Set<String> = .init()
+
+for _ in 0..<N {
+    set.insert(readLine()!)
+}
+
+let sorted = Array(set).sorted { $0.count == $1.count ? $0 < $1 : $0.count < $1.count }
+print(sorted.joined(separator: "\n"))
+*/
