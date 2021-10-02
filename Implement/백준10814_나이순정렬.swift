890f989ac711470daa2059a42b795a5a5432b7db
Kim Min Seop <msk7529@naver.com>
2021-10-03 02:57:28 +0900

    백준 10814번(나이순 정렬) - 구현. 정렬

---
+++ Implement/백준10814_나이순정렬.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/03.
+//
+/*
+typealias Person = (Int, Int, String)
+let N = Int(readLine()!)!
+var arr: [Person] = []
+var result: String = ""
+
+for i in 0..<N {
+    let line = readLine()!.split(separator: " ").map { String($0) }
+    arr.append(Person(i, Int(line[0])!, line[1]))
+}
+
+arr.sort(by: { $0.1 == $1.1 ? $0.0 < $1.0 : $0.1 < $1.1 })
+
+for curr in arr {
+    result += "\(curr.1) \(curr.2)\n"
+}
+result.removeLast()
+print(result)
+*/
