ec65819ea3fcfc6ca045518dcb23bd55492f61a1
Kim Min Seop <msk7529@naver.com>
2021-10-03 02:25:44 +0900

    백준 1850번(최대공약수) - 수학

---
+++ Math/백준1850_최대공약수.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/03.
+//
+/*
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+let A = input[0], B = input[1]
+let g = gcd(A, B)
+print(String(repeating: "1", count: g))
+*/
