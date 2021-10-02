456fc3809657b2fef20c79cc07ef83d808307410
Kim Min Seop <msk7529@naver.com>
2021-10-03 03:18:01 +0900

    백준 18870번(좌표 압축) - 구현. 정렬. 좌표 압축

---
+++ Implement/백준18870_좌표압축.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/03.
+//
+/*
+let N = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+var dic: [Int: Int] = [:]
+var result: String = ""
+
+var index = 0
+for curr in Array(Set(arr)).sorted() {
+    dic[curr] = index
+    index += 1
+}
+
+for curr in arr {
+    result += String(dic[curr]!) + " "
+}
+result.removeLast()
+print(result)
+*/
