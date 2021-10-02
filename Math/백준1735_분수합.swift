2a18372549037257d5121948214aea839ecc9051
Kim Min Seop <msk7529@naver.com>
2021-10-03 01:48:18 +0900

    백준 1735번(분수 합) - 수학

---
+++ Math/백준1735_분수합.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/03.
+//
+/*
+let input1 = readLine()!.split(separator: " ").map { Int(String($0))! }
+let input2 = readLine()!.split(separator: " ").map { Int(String($0))! }
+let A1 = input1[0], B1 = input1[1], A2 = input2[0], B2 = input2[1]
+
+var B = lcm(B1, B2)
+var A = A1 * (B / B1) + A2 * (B / B2)
+let g = gcd(A, B)
+B /= g
+A /= g
+print("\(A) \(B)")
+*/
