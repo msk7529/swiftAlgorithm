9f2b8868fab4222534a83af16cc494658f6e7f2e
Kim Min Seop <msk7529@naver.com>
2021-10-07 02:38:44 +0900

    백준 11868번(님 게임 2) - 게임이론. 스프라그 그런디(Sprague-Grundy) 정리 이용.. 인데 이론이 어려워서 아직 완전히 이해못함

---
+++ GameTheory/Sprague-Grundy/백준11868_님게임2.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/07.
+//
+/* 스프라그 그런디 참고자료
+https://driip.me/42a37c2a-fe1b-4c65-b7c4-941ee2deab38
+https://casterian.net/archives/1239
+https://tataky.tistory.com/2
+*/
+/*
+let N = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+var result: Int = 0
+
+for curr in arr {
+    // 돌이 p개 있는 더미의 그런디 넘버는 p 그 자체이다.
+    result ^= curr
+}
+
+// 초기 그런디 넘버들을 모두 XOR한 결과값이 0이 아니면 선공이 이긴다.
+print(result == 0 ? "cubelover" : "koosaga")
+*/
