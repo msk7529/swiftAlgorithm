aa3bb65eeec14a3551a11a0491479bf583df9336
Kim Min Seop <msk7529@naver.com>
2021-10-09 06:06:08 +0900

    백준 15904번(UCPC는 무엇의 약자일까?) - 구현. 문자열

---
+++ Implement/백준15904_UCPC는무엇의약자일까.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/09.
+//
+/*
+let ucpc: [String] = "UCPC".map { String($0) }
+var targetIndex: Int = 0
+for char in readLine()!.map({ String($0) }) {
+    if targetIndex == ucpc.count { break }
+    if ucpc[targetIndex] == char {
+        targetIndex += 1
+    }
+}
+print(targetIndex == ucpc.count ? "I love UCPC" : "I hate UCPC")
+*/
