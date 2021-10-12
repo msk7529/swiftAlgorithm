03c372db5507172a017b037d36df99a9df27ab76
Kim Min Seop <msk7529@naver.com>
2021-10-13 04:09:12 +0900

    백준 1076번(저항) - 구현

---
+++ Implement/백준1076_저항.swift
@@ -0,0 +1,36 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/13.
+//
+/*
+var dic: [String: (Int, Int)] = [:]
+var currValue: Int = 0
+var currMultiValue: Int = 1
+var result: Int = 0
+
+func addValue(_ color: String) {
+    dic[color] = (currValue, currMultiValue)
+    currValue += 1
+    currMultiValue *= 10
+}
+
+addValue("black")
+addValue("brown")
+addValue("red")
+addValue("orange")
+addValue("yellow")
+addValue("green")
+addValue("blue")
+addValue("violet")
+addValue("grey")
+addValue("white")
+
+let first = readLine()!
+let second = readLine()!
+let third = readLine()!
+let value = Int(exactly: dic[first]!.0)! * 10 + Int(exactly: dic[second]!.0)!
+result = value * Int(dic[third]!.1)
+print(result)
+*/
