3d4d110a58a06e9cc10f47854bfd424d0cab6e97
Kim Min Seop <msk7529@naver.com>
2021-10-15 01:00:51 +0900

    백준 2693번(N번째 큰 수) - 구현

---
+++ Implement/백준2693_N번째큰수.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/15.
+//
+/*
+let T = Int(readLine()!)!
+var result: String = ""
+
+for _ in 0..<T {
+    let arr = readLine()!.split(separator: " ").map { Int(String($0))! }.sorted { $0 > $1 }
+    result += "\(arr[2])\n"
+}
+result.removeLast()
+print(result)
+*/
