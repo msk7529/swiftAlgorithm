20faab4523b82f56defa3c5243a68fcdb4fb2ae4
Kim Min Seop <msk7529@naver.com>
2021-10-16 23:18:42 +0900

    백준 11279번(최대 힙) - 구현

---
+++ Implement/백준11279_최대힙.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/16.
+//
+/*
+var pq: PriorityQueue<Int> = .init(comparer: >)
+var result: String = ""
+for _ in 0..<Int(readLine()!)! {
+    let num = Int(readLine()!)!
+    if num == 0 {
+        if let top = pq.dequeue() {
+            result += "\(top)\n"
+        } else {
+            result += "0\n"
+        }
+    } else {
+        pq.enqueue(num)
+    }
+}
+print(result)
+*/
