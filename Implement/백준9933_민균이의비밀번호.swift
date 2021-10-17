0affb513f966f1e79ec9f7fe205380b20fea5a71
Kim Min Seop <msk7529@naver.com>
2021-10-17 22:19:50 +0900

    백준 9933번(민균이의 비밀번호) - 구현. 문자열

---
+++ Implement/백준9933_민균이의비밀번호.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/17.
+//
+/*
+let N = Int(readLine()!)!
+var arr: [String] = []
+
+for _ in 0..<N {
+    arr.append(readLine()!)
+}
+
+for curr in arr {
+    let reverse = String(curr.reversed())
+    if arr.contains(reverse) {
+        let len = reverse.count
+        let char = Array(curr)[len / 2]
+        print("\(len) \(char)")
+        break
+    }
+}
+*/
