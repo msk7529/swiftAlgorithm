a1f012aa709aac9340b152989e112108d9afa387
Kim Min Seop <msk7529@naver.com>
2021-10-18 23:37:42 +0900

    백준 2592번(대표값) - 구현

---
+++ Implement/백준2592_대표값.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/18.
+//
+/*
+var arr: [Int] = []
+var dic: [Int: Int] = [:]
+
+for _ in 0..<10 {
+    let num = Int(readLine()!)!
+    arr.append(num)
+    dic[num, default: 0] += 1
+}
+
+print(arr.reduce(0, +) / 10)
+print(dic.sorted { $0.value > $1.value }.first?.key ?? 0)
+*/
