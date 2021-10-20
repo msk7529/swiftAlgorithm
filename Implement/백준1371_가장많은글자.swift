8b61a54717a5aeea03d86cc78393f3b756bc957b
Kim Min Seop <msk7529@naver.com>
2021-10-20 22:41:21 +0900

    백준 1371번(가장 많은 글자) - 구현

---
+++ Implement/백준1371_가장많은글자.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/20.
+//
+/*
+var dic: [Character: Int] = [:]
+
+while let line = readLine() {
+    line.filter { $0.isLowercase }.forEach { dic[$0, default: 0] += 1 }
+}
+let maxValue = dic.values.max()!
+print(dic.filter { $0.value == maxValue }.keys.map { String($0) }.sorted().joined())
+*/
