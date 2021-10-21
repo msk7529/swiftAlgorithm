cda40dd42bdc00783efc16011e58a889ecd1ab8a
Kim Min Seop <msk7529@naver.com>
2021-10-21 23:54:38 +0900

    백준 23027번(1번부터 문제의 상태가...?) - 구현

---
+++ Implement/백준23027_1번부터문제의상태가.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/21.
+//
+/*
+let letter = readLine()!
+var result: String = ""
+
+if letter.contains("A") {
+    result = letter.map { ["B", "C", "D", "F"].contains(String($0)) ? "A" : String($0) }.joined()
+} else if letter.contains("B") {
+    result = letter.map { ["C", "D", "F"].contains(String($0)) ? "B" : String($0) }.joined()
+} else if letter.contains("C") {
+    result = letter.map { ["D", "F"].contains(String($0)) ? "C" : String($0) }.joined()
+} else {
+    result = String(repeating: "A", count: letter.count)
+}
+print(result)
+*/
