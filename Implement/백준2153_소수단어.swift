57fc1d27312aff5c64ed61d674d002229da9493b
Kim Min Seop <msk7529@naver.com>
2021-10-24 23:16:32 +0900

    백준 2153번(소수 단어) - 구현

---
+++ Implement/백준2153_소수단어.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/24.
+//
+/*
+import Foundation
+
+let word = readLine()!
+var sum: Int = 0
+
+for char in word {
+    sum += char.isLowercase ? Int(char.asciiValue! - 96) : Int(char.asciiValue! - 64)
+}
+print(sum.isPrime() || word == "a" ? "It is a prime word." : "It is not a prime word.")
+*/
