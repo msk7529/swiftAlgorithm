851022a9e3811ff2be8d5f2df2eea1a8bd70aa27
Kim Min Seop <msk7529@naver.com>
2021-10-28 23:31:36 +0900

    백준 5789번(한다 안한다) - 구현

---
+++ Implement/백준5789_한다안한다.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/28.
+//
+/*
+for _ in 0..<Int(readLine()!)! {
+    let str = readLine()!.map { String($0) }
+    let len = str.count
+    if str[len / 2] == str[len / 2 - 1] {
+        print("Do-it")
+    } else {
+        print("Do-it-Not")
+    }
+}
+*/
