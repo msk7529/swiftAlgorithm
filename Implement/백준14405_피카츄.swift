1c73143297225f1ad3eb3033036685a6e8930744
Kim Min Seop <msk7529@naver.com>
2021-10-30 23:50:24 +0900

    백준 14405번(피카츄) - 구현

---
+++ Implement/백준14405_피카츄.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/30.
+//
+/*
+import Foundation
+
+let word = readLine()!
+if word.isEmpty {
+    print("NO")
+} else {
+    let result = word.replacingOccurrences(of: "pi", with: "-").replacingOccurrences(of: "ka", with: "-").replacingOccurrences(of: "chu", with: "-")
+    print(result.replacingOccurrences(of: "-", with: "").isEmpty ? "YES" : "NO")
+}
+*/
