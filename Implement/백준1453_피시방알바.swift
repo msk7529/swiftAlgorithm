0e1b22a44061e2a426c5551a05e8f8701b59689a
Kim Min Seop <msk7529@naver.com>
2021-10-31 23:54:12 +0900

    백준 1453번(피시방 알바) - 구현

---
+++ Implement/백준1453_피시방알바.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/10/31.
+//
+/*
+let N  = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+var set: Set<Int> = .init()
+var result = 0
+
+for num in arr {
+    if set.contains(num) {
+        result += 1
+    } else {
+        set.insert(num)
+    }
+}
+
+print(result)
+*/
--- main.swift
+++ main.swift
@@ -2,5 +2,5 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by MinSeop on 2021/10/30.
+//  Created by MinSeop on 2021/10/31.
 //
