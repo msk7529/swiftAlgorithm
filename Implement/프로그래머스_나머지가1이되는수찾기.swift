030d4011044eefd825a16432962cc1c31d0fda63
Kim Min Seop <msk7529@naver.com>
2021-11-02 00:52:31 +0900

    프로그래머스 월간 코드 챌린지 시즌3(나머지가 1이 되는 수 찾기) - 구현

---
+++ Implement/프로그래머스_나머지가1이되는수찾기.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/11/02.
+//
+/*
+func solution(_ n: Int) -> Int {
+    for num in 2..<n {
+        if n % num == 1 {
+            return num
+        }
+    }
+    return 0
+}
+
+print(solution(10))
+*/
