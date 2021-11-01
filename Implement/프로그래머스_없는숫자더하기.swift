01d949ee8723c03a4cafbee47a196a15f108fb75
Kim Min Seop <msk7529@naver.com>
2021-11-01 23:01:30 +0900

    프로그래머스 월간 코드 챌린지 시즌3(없는 숫자 더하기) - 구현

---
+++ Implement/프로그래머스_없는숫자더하기.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/11/01.
+//
+/*
+func solution(_ numbers: [Int]) -> Int {
+    var set: Set<Int> = Set([0,1,2,3,4,5,6,7,8,9])
+    for number in numbers {
+        set.remove(number)
+    }
+        
+    return Array(set).reduce(0,+)
+}
+
+print(solution([1,2,3,4,6,7,8,0]))
+*/
