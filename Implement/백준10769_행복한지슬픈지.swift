3fa9a254592235ffee44bfdfbcbb68c7c5c1f438
Kim Min Seop <msk7529@naver.com>
2021-11-07 23:27:51 +0900

    백준 10769번(행복한지 슬픈지) - 구현

---
+++ Implement/백준10769_행복한지슬픈지.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/11/07.
+//
+/*
+import Foundation
+
+let line = readLine()! + "a"
+let happy = line.components(separatedBy: ":-)")
+let sad = line.components(separatedBy: ":-(")
+
+if happy.count == 1 && sad.count == 1 {
+    print("none")
+} else if happy.count > sad.count {
+    print("happy")
+} else if happy.count < sad.count {
+    print("sad")
+} else {
+    print("unsure")
+}
+*/
--- main.swift
+++ main.swift
@@ -2,5 +2,5 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by MinSeop on 2021/11/06.
+//  Created by MinSeop on 2021/11/07.
 //
