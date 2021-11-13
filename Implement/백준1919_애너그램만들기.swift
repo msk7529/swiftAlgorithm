891050bb6e4e86a77232e0613be06e86a1eafc0f
Kim Min Seop <msk7529@naver.com>
2021-11-13 23:45:34 +0900

    백준 1919번(애너그램 만들기) - 구현

---
+++ Implement/백준1919_애너그램만들기.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/11/13.
+//
+/*
+let word1 = readLine()!.sorted()
+let word2 = readLine()!.sorted()
+let diff = word1.difference(from: word2)
+print(diff.count)
+*/
--- main.swift
+++ main.swift
@@ -2,5 +2,5 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by MinSeop on 2021/11/07.
+//  Created by MinSeop on 2021/11/13.
 //
