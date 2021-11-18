380452491a1bd8963250343f0589fe81a991f354
Kim Min Seop <msk7529@naver.com>
2021-11-19 02:01:50 +0900

    백준 11944번(NN) - 구현

---
+++ Implement/백준11944_NN.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/11/19.
+//
+/*
+let line = readLine()!.split(separator: " ").map { String($0) }
+let N = line[0], M = line[1]
+let fullStr = String(repeating: N, count: Int(N)!)
+if fullStr.count > Int(M)! {
+    print(fullStr.prefix(Int(M)!))
+} else {
+    print(fullStr)
+}
+*/
