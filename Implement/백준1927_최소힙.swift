73270080bc4ac4b0034b98a02083856b7bf07a3c
Kim Min Seop <msk7529@naver.com>
2021-11-26 02:39:11 +0900

    백준 1927번(최소 힙) - 구현

---
+++ Implement/백준1927_최소힙.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/11/26.
+//
+/*
+let N = Int(readLine()!)!
+var queue: PriorityQueue<Int> = .init(comparer: <)
+var result: String = ""
+
+for _ in 0..<N {
+    let num = Int(readLine()!)!
+    if num == 0 {
+        if let front = queue.dequeue() {
+            result += "\(front)\n"
+        } else {
+            result += "0\n"
+        }
+    } else {
+        queue.enqueue(num)
+    }
+}
+print(result)
+*/
