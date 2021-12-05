fbe353110aca5938e50146aed4c53b7bd9a96271
Kim Min Seop <msk7529@naver.com>
2021-12-05 23:31:51 +0900

    백준 14490번(백대열) - 구현

---
+++ Implement/백준14490_백대열.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/12/5.
+//
+/*
+let numbers = readLine()!.split(separator: ":").map { Int(String($0))! }
+let a = numbers[0], b = numbers[1], g = gcd(a, b)
+print("\(a/g):\(b/g)")
+*/
