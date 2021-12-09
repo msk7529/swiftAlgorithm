c444648e60db0d663ed2326d81a8c5fc0f126c96
Kim Min Seop <msk7529@naver.com>
2021-12-09 23:43:52 +0900

    백준 4999번(아!) - 구현

---
+++ Implement/백준4999_아!.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/12/8.
+//
+/*
+import Foundation
+
+let user = readLine()!
+let doctor = readLine()!
+print(user.contains(doctor) ? "go" : "no")
+*/
