1594043d9d7bb78c6ecc0540b7cf7ecfb0ee2102
Kim Min Seop <msk7529@naver.com>
2021-12-11 23:23:02 +0900

    백준 2711번(오타맨 고창영) - 구현

---
+++ Implement/백준2711_오타맨고창영.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/12/11.
+//
+/*
+let T = Int(readLine()!)!
+var result: String = ""
+
+for _ in 0..<T {
+    let line = readLine()!.split(separator: " ").map { String($0) }
+    let index = Int(line[0])! - 1
+    var word = line[1]
+    word.remove(at: String.Index(utf16Offset: index, in: word))
+    result += "\(word)\n"
+}
+result.removeLast()
+print(result)
+*/
