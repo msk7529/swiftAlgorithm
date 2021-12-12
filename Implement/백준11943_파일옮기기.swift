9ac869a8a947d5f65408accfbe804b2945cebb73
Kim Min Seop <msk7529@naver.com>
2021-12-12 23:43:01 +0900

    백준 11943번(파일 옮기기) - 구현

---
+++ Implement/백준11943_파일옮기기.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/12/12.
+//
+/*
+let line1 = readLine()!.split(separator: " ").map { Int(String($0))! }
+let line2 = readLine()!.split(separator: " ").map { Int(String($0))! }
+let A = line1[0], B = line1[1], C = line2[0], D = line2[1]
+print(min(A + D, B + C))
+*/
--- main.swift
+++ main.swift
@@ -2,5 +2,5 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by MinSeop on 2021/12/11.
+//  Created by MinSeop on 2021/12/12.
 //
