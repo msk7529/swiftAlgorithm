447455d09a24434bdc34599b0cee0aa4f0198733
Kim Min Seop <msk7529@naver.com>
2021-12-13 23:54:28 +0900

    백준 1302번(베스트셀러) - 구현

---
+++ Implement/백준1302_베스트셀러.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/12/13.
+//
+/*
+let N = Int(readLine()!)!
+var dic: [String: Int] = [:]
+var result: [String] = []
+
+for _ in 0..<N {
+    let word = readLine()!
+    dic[word, default: 0] += 1
+}
+
+let maxValue = dic.values.max() ?? 0
+
+for (key, value) in dic {
+    if value == maxValue {
+        result.append(key)
+    }
+}
+result.sort()
+print(result.first!)
+*/
