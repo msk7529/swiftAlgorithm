601eb07e9e92466379e8dbe3da5ff1e98b2084eb
Kim Min Seop <msk7529@naver.com>
2021-12-16 23:48:58 +0900

    백준 9085번(더하기) - 구현

---
+++ Implement/백준9085_더하기.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/12/16.
+//
+/*
+let T = Int(readLine()!)!
+for _ in 0..<T {
+    let _ = Int(readLine()!)!
+    let numbers = readLine()!.split(separator: " ").map { Int(String($0))! }
+    print(numbers.reduce(0, +))
+}
+*/
