e7767495d99b7d9b93d4d0c8da88b4d95dbb7be1
Kim Min Seop <msk7529@naver.com>
2021-12-23 23:43:07 +0900

    백준 1075번(나누기) - 구현

---
+++ Implement/백준1075_나누기.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/12/23.
+//
+/*
+let N = Int(readLine()!)!
+let F = Int(readLine()!)!
+let tmp = (N / 100) * 100
+
+for i in 0..<100 {
+    let curr = tmp + i
+    if curr.isMultiple(of: F) {
+        var result = "0\(i)"
+        if result.count == 3 {
+            result.removeFirst()
+        }
+        print(result)
+        break
+    }
+}
+*/
