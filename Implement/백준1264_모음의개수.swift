fb84edbf25adfeab3aeb7e06b9c0b025b06d7cdd
Kim Min Seop <msk7529@naver.com>
2021-12-25 23:31:21 +0900

    백준 1264번(모음의 개수) - 구현

---
+++ Implement/백준1264_모음의개수.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/12/25.
+//
+/*
+let words = ["a", "e", "i", "o", "u"]
+while true {
+    let line = readLine()!.lowercased()
+    if line == "#" { break }
+    let num = line.filter { words.contains(String($0)) }.count
+    print(num)
+}
+*/
