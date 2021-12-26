eddb27a9a2d5459fd0b33f5d2a7299295fa0b595
Kim Min Seop <msk7529@naver.com>
2021-12-26 23:55:19 +0900

    백준 11728번(배열 합치기) - 구현. 시간초과... 정렬부분을 줄여야 하나 ?

---
+++ Implement/백준11728_배열합치기.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2021/12/26.
+//
+/*
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+let arr1 = readLine()!.split(separator: " ").map { Int(String($0))! }
+let arr2 = readLine()!.split(separator: " ").map { Int(String($0))! }
+let arr = (arr1 + arr2).sorted()
+var result: String = ""
+for num in arr {
+    result += "\(num) "
+}
+result.removeLast()
+print(result)
+*/
