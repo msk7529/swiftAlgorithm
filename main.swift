2a949ecbab84b6bedd3c8b80931850ff7fa1b7d8
Kim Min Seop <msk7529@naver.com>
2022-01-01 23:56:22 +0900

    백준 2145번(숫자 놀이) - 구현. 아직 오답

--- main.swift
+++ main.swift
@@ -2,5 +2,17 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by MinSeop on 2021/12/26.
+//  Created by MinSeop on 2022/01/01.
 //
+while true {
+    var num = Int(readLine()!)!
+    if num == 0 { break }
+    
+    var result: Int = 0
+    
+    while num >= 10 {
+        result += (num % 10)
+        num /= 10
+    }
+    print(num)
+}
