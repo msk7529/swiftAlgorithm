c1ec57be80821c611cf923e82bd195c2beb28a85
Kim Min Seop <msk7529@naver.com>
2022-01-06 02:12:42 +0900

    백준 24039번(2021은 무엇이 특별할까?) - 구현

---
+++ Implement/백준24039_2021은무엇이특별할까.swift
@@ -0,0 +1,30 @@
+//
+//  GetPrimeList.swift
+//  Algorithm
+//
+//  Created by kakao on 2021/05/11.
+//
+/*
+ let N = Int(readLine()!)!
+ let chae = getPrimeList(to: 150)
+ var primeArr: [Int] = []
+
+ for (index, value) in chae.enumerated() {
+     if value {
+         primeArr.append(index)
+     }
+ }
+ var arrWithPrimes: [Int] = []
+
+ for i in 0..<primeArr.count-1 {
+     arrWithPrimes.append(primeArr[i] * primeArr[i + 1])
+ }
+
+ for num in arrWithPrimes {
+     if num > N {
+         print(num)
+         break
+     }
+ }
+
+*/
--- main.swift
+++ main.swift
@@ -2,5 +2,5 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by MinSeop on 2022/01/02.
+//  Created by MinSeop on 2022/01/06.
 //
