0f23b60bd48955c7403e8ce805010b4ffc56c89a
Kim Min Seop <msk7529@naver.com>
2022-01-11 21:55:23 +0900

    백준 1267번(핸드폰 요금) - 구현

---
+++ Implement/백준1267_핸드폰요금.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2022/01/11
+/*
+let N = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+var sum1 = 0, sum2 = 0
+
+for num in arr {
+    sum1 += ((num / 30) + 1) * 10
+    sum2 += ((num / 60) + 1) * 15
+}
+
+if sum1 < sum2 {
+    print("Y \(sum1)")
+} else if sum1 == sum2 {
+    print("Y M \(sum1)")
+} else {
+    print("M \(sum2)")
+}
+*/
