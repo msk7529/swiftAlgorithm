265c7c6e9af264525cf1920e4033a339c409fc07
Kim Min Seop <msk7529@naver.com>
2022-01-14 23:45:28 +0900

    백준 1526번(가장 큰 금민수) - 구현

---
+++ Implement/백준1526_가장큰금민수.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2022/01/14.
+
+/*
+let N = Int(readLine()!)!
+
+for num in stride(from: N, through: 4, by: -1) {
+    if check(num: num) {
+        print(num)
+        break
+    }
+}
+
+func check(num: Int) -> Bool {
+    var n = num
+    while n > 0 {
+        if n % 10 != 7 && n % 10 != 4 {
+            return false
+        }
+        n /= 10
+    }
+    return true
+}
+*/
--- main.swift
+++ main.swift
@@ -2,4 +2,5 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by MinSeop on 2022/01/12.
+//  Created by MinSeop on 2022/01/14.
+
