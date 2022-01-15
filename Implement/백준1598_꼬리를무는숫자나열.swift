4fd9785c729ea768fe035b30fdb0ac6d079cd5a7
Kim Min Seop <msk7529@naver.com>
2022-01-15 23:23:09 +0900

    백준 1598번(꼬리를 무는 숫자배열) - 구현

---
+++ Implement/백준1598_꼬리를무는숫자나열.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2022/01/15.
+/*
+import Foundation
+
+let line = readLine()!.split(separator: " ").map { Int(String($0))! }
+let (y1, x1) = calcPos(num: line[0])
+let (y2, x2) = calcPos(num: line[1])
+
+print(abs(y1 - y2) + abs(x2 - x1))
+
+func calcPos(num: Int) -> (Int, Int) {
+    let x = num.isMultiple(of: 4) ? num / 4 - 1 : num / 4
+    var y = (num % 4) - 1
+    if y < 0 { y = 3 }
+    return (y, x)
+}
+*/
