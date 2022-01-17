09d136b29f3d1e11e42608cc1c1e82bfd8b00797
Kim Min Seop <msk7529@naver.com>
2022-01-17 23:52:52 +0900

    백준 2566번(최댓값) - 구현

---
+++ Implement/백준2566_최댓값.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2022/01/17.
+/*
+var arr: [[Int]] = Array(repeating: Array(repeating: 0, count: 10), count: 10)
+var maxNum: Int = 0
+var y: Int = 0, x = 0
+for i in 1...9 {
+    let line = [0] + readLine()!.split(separator: " ").map { Int(String($0))! }
+    arr[i] = line
+}
+
+for i in 1...9 {
+    for j in 1...9 {
+        if maxNum < arr[i][j] {
+            maxNum = arr[i][j]
+            y = i
+            x = j
+        }
+    }
+}
+
+print(maxNum)
+print("\(y) \(x)")
+*/
