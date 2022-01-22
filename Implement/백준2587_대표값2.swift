22f3f8eda7034ee9ff47d3fbc860859e1c215bc3
Kim Min Seop <msk7529@naver.com>
2022-01-22 23:33:26 +0900

    백준 2587번(대표값2) - 구현

---
+++ Implement/백준2587_대표값2.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2022/01/22.
+/*
+var arr: [Int] = []
+for _ in 0..<5 {
+    arr.append(Int(readLine()!)!)
+}
+arr.sort()
+print(arr.reduce(0, +) / 5)
+print(arr[2])
+*/
