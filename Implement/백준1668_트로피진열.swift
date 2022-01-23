5aaa56d4e60b1debc1147d219cd84190b77df2b4
Kim Min Seop <msk7529@naver.com>
2022-01-24 03:23:46 +0900

    백준 1668번(트로피 진열) - 구현

---
+++ Implement/백준1668_트로피진열.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2022/01/24.
+/*
+let N = Int(readLine()!)!
+var arr: [Int] = []
+var result1 = 0, result2 = 0, max1 = 0, max2 = 0
+
+for _ in 0..<N {
+    arr.append(Int(readLine()!)!)
+}
+
+for num in arr {
+    if num > max1 {
+        max1 = num
+        result1 += 1
+    }
+}
+
+for num in arr.reversed() {
+    if num > max2 {
+        max2 = num
+        result2 += 1
+    }
+}
+print("\(result1)\n\(result2)")
+*/
