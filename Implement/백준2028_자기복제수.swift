48584870b95ec49564b3db94eecd5a20787718e0
Kim Min Seop <msk7529@naver.com>
2022-01-23 22:30:30 +0900

    백준 2028번(자기복제수) - 구현

---
+++ Implement/백준2028_자기복제수.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2022/01/23.
+/*
+for _ in 0..<Int(readLine()!)! {
+    let num = Int(readLine()!)!
+    let powStr = String(num * num)
+    print(powStr.hasSuffix(String(num)) ? "YES" : "NO")
+}
+*/
