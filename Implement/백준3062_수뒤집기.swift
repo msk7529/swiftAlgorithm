0ad510089b9298c8288d67c158875e4346bb1d54
Kim Min Seop <msk7529@naver.com>
2022-01-27 23:45:04 +0900

    백준 3062번(수 뒤집기) - 구현

---
+++ Implement/백준3062_수뒤집기.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2022/01/27.
+/*
+for _ in 0..<Int(readLine()!)! {
+    let origin = readLine()!
+    let reverse = String(origin.reversed())
+    let sum = Int(origin)! + Int(reverse)!
+    let num1 = String(sum)
+    let num2 = String(num1.reversed())
+    print(num1 == num2 ? "YES" : "NO")
+}
+*/
--- main.swift
+++ main.swift
@@ -2,6 +2,5 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by MinSeop on 2022/01/26.
-
+//  Created by MinSeop on 2022/01/27.
 
