4f63120788512ce18beba0b9ae79f9dd39f7dfe3
Kim Min Seop <msk7529@naver.com>
2022-02-01 23:44:53 +0900

    백준 2965번(캥거루 세마리) - 구현

---
+++ Implement/백준2965_캥거루세마리.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2022/02/01.
+/*
+let numbers = readLine()!.split(separator: " ").map { Int(String($0))! }
+let tmp1 = numbers[1] - numbers[0] - 1, tmp2 = numbers[2] - numbers[1] - 1
+print(max(tmp1, tmp2))
+*/
