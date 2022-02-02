1c9fdb846694b830f77ef204f6abcef050389313
Kim Min Seop <msk7529@naver.com>
2022-02-02 23:46:51 +0900

    백준 1964번(오각형, 오각형, 오각형...) - 구현

---
+++ Implement/백준1964_오각형오각형오각형.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2022/02/02.
+/*
+let N = Int(readLine()!)!
+var curr: Int = 5
+
+for i in stride(from: 2, through: N, by: 1) {
+    curr = (curr + 3 * i + 1) % 45678
+}
+print(curr)
+*/
