4e9e3fc358661fa322737a6abe7f07232d20aacc
Kim Min Seop <msk7529@naver.com>
2022-03-02 22:50:39 +0900

    no message

--- main.swift
+++ main.swift
@@ -3,4 +3,10 @@
 //  Algorithm
 //
 //  Created by MinSeop on 2022/02/28.
+import Foundation
 
+let date = Date()
+let test = date.addingTimeInterval(300)
+print(date)
+print(test)
+print(test >= date)
