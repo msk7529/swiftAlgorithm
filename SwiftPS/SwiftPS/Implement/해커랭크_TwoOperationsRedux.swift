3134e43df465537f44e438761422efaea752b19a
Kim Min Seop <msk7529@naver.com>
2022-08-23 04:07:37 +0900

    해커랭크 216361(Two Operations Redux) - 구현

---
+++ SwiftPS/SwiftPS/Implement/해커랭크_TwoOperationsRedux.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2022/08/23
+
+/*
+ 수직선 생각하면 금방 풀 수 있음. 1번 난이도?
+ */
+
+/*
+func findMaxNum(x: Int, y: Int, z: Int) -> Int {
+    let diff = abs(x - y)
+    if z < diff {
+        return -1
+    }
+    return max(x, y) + (z - diff) / 2
+}
+
+print(findMaxNum(x: 8, y: 5, z: 3))
+print(findMaxNum(x: 4, y: 4, z: 6))
+*/
