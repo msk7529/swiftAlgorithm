6ebd47d6f79d0b1da64ad788be98aea92d31bbb1
Kim Min Seop <msk7529@naver.com>
2022-08-23 03:50:40 +0900

    해커랭크 620610(Restocking the Warehouse) - 구현

---
+++ SwiftPS/SwiftPS/Implement/해커랭크_RestockingTheWarehouse.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by MinSeop on 2022/08/23
+
+/*
+ 매우 쉬운 단순 구현문제.
+ */
+
+/*
+func restock(itemCount: [Int], target: Int) -> Int {
+    var sum = 0
+    for num in itemCount {
+        sum += num
+        if sum >= target {
+            break
+        }
+    }
+    return abs(target - sum)
+}
+
+print(restock(itemCount: [6, 1, 2, 1] , target: 100))
+*/
